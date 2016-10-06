62a2a8528858fd1cd41401b3455662993494e7c1
InSung Jung <hunter10@naver.com>
2016-10-06 18:05:27 +0900

    Revert "Program.cs on Local repository"
    This reverts commit 4489daa112888b4201cfc7cb50a6d6c82fc6bd6f.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -14,7 +14,6 @@ namespace HelloWorld
             Console.WriteLine("World is mine");
             Console.WriteLine("Earth is mine");
             Console.WriteLine("Luna is mine");
-            Console.WriteLine("Earth on Local Repository");
         }
     }
 }
